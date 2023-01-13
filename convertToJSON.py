e78d6073501efc581c52f2960dbcdd28ef0e1faf
Paula Ontalvilla <paulaonta@gmail.com>
2023-01-14 00:32:11 +0100

    estazio guztien informazioa lortuta

--- convertToJSON.py
+++ convertToJSON.py
@@ -4,7 +4,7 @@ import  json
 text = {
     "license": "CC0-1.0",
     "description": {
-        "eu": "Pamplonako datu meteorologikoak"
+        "eu": " Datu meteorologikoak"
     },
     "schema": {
         "fields": [
@@ -22,18 +22,11 @@ text = {
                     "eu": "Tenperatura maximoa"
                 }
             },
-            {
-                "name": "avgHighTemp",
+{
+                "name": "highTempAvg",
                 "type": "number",
                 "title": {
                     "eu": "Tenperatura maximoaren batezbestekoa"
-                }
-            },
-            {
-                "name": "avgLowTemp",
-                "type": "number",
-                "title": {
-                    "eu": "Tenperatura minimoaren batezbestekoa"
                 }
             },
             {
@@ -43,11 +36,138 @@ text = {
                     "eu": "Tenperatura minimoa"
                 }
             },
+{
+                "name": "highTempAvg",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura minimoaren batezbestekoa"
+                }
+            },
             {
                 "name": "precip",
                 "type": "number",
                 "title": {
-                    "eu": "Prezipitazioak"
+                    "eu": "Pilatutako prezipitazioak"
+                }
+            }
+        ]
+    },
+}
+
+text = {
+    "license": "CC0-1.0",
+    "description": {
+        "eu": " Datu meteorologikoak"
+    },
+    "schema": {
+        "fields": [
+            {
+                "name": "date",
+                "type": "string",
+                "title": {
+                    "ar": "data"
+                }
+            },
+            {
+                "name": "highTemp",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura maximoa"
+                }
+            },
+{
+                "name": "highTempAvg",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura maximoaren batezbestekoa"
+                }
+            },
+            {
+                "name": "lowTemp",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura minimoa"
+                }
+            },
+{
+                "name": "highTempAvg",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura minimoaren batezbestekoa"
+                }
+            },
+            {
+                "name": "precip",
+                "type": "number",
+                "title": {
+                    "eu": "Pilatutako prezipitazioak"
+                }
+            }
+        ]
+    },
+}
+
+text2 = {
+    "license": "CC0-1.0",
+    "description": {
+        "eu": " Datu meteorologikoak"
+    },
+    "schema": {
+        "fields": [
+            {
+                "name": "date",
+                "type": "string",
+                "title": {
+                    "ar": "data"
+                }
+            },
+            {
+                "name": "avgTemp",
+                "type": "number",
+                "title": {
+                    "eu": "Batazbesteko tenperatura"
+                }
+            },
+            {
+                "name": "highTemp",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura maximoa"
+                }
+            },
+{
+                "name": "highTempAvg",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura maximoaren batezbestekoa"
+                }
+            },
+            {
+                "name": "lowTemp",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura minimoa"
+                }
+            },
+{
+                "name": "highTempAvg",
+                "type": "number",
+                "title": {
+                    "eu": "Tenperatura minimoaren batezbestekoa"
+                }
+            },
+            {
+                "name": "hez",
+                "type": "number",
+                "title": {
+                    "eu": "Batazbesteko hezetasuna"
+                }
+            },
+            {
+                "name": "precip",
+                "type": "number",
+                "title": {
+                    "eu": "Pilatutako prezipitazioak"
                 }
             }
         ]
@@ -56,17 +176,20 @@ text = {
 
 def convert_toJSON(directory, files):
     rows = []
+    month = "01"
+    pos_max = 1
+    pos_min = 2
+    pos_prez = 3
     for f in files:
-        year = str(f).split(".csv")[0].split("/")[-1]
         # open csv files
         mycsv = csv.reader(open(f, encoding='latin-1'))
 
         first = True
         max_tenp = -float("Inf")
         min_tenp = float("Inf")
-        bb_max = 0.0
-        bb_min = 0.0
-        bb_prez = 0.0
+        bb_tenp_max = 0.0
+        bb_tenp_min = 0.0
+        prez = 0.0
         cont = 0
 
         for line in mycsv:
@@ -77,31 +200,181 @@ def convert_toJSON(directory, files):
                 #get all element in an array
                 all_element = "".join(line).split(";") #[0] data, [1] max. temp, [2] min. temp, [3] prez.
                 all_element = ['0.0' if elem == '' else elem for elem in all_element]
-                if max_tenp < float(all_element[1]):
-                    max_tenp = float(all_element[1])
-                if min_tenp > float(all_element[2]):
-                    min_tenp = float(all_element[2])
-                bb_max += float(all_element[1])
-                bb_min += float(all_element[2])
-                bb_prez += float(all_element[3])
-        bb_max /= cont
-        bb_min /= cont
-        bb_prez /= cont
-        if max_tenp == 0.0:
-            max_tenp = 'null'
-        if min_tenp == 0.0:
-            min_tenp = 'null'
-        if bb_max == 0.0:
-            bb_max = 'null'
-        if bb_min == 0.0:
-            bb_min = 'null'
-        if bb_prez == 0.0:
-            bb_prez = 'null'
-        rows.append([year, max_tenp, bb_max, bb_min, min_tenp, bb_prez])
+                year = all_element[0].split(" ")[0].split("/")[-1]
+                prev_month = month
+                month = all_element[0].split(" ")[0].split("/")[1]
+
+                if len(all_element) == 3:
+                    print("it is not complete file: " +str(f))
+                    pos_min = 1
+                    pos_prez = 2
+                else:
+                    if max_tenp < float(all_element[pos_max]):
+                        max_tenp = float(all_element[pos_max])
+                    bb_tenp_max += float(all_element[pos_max])
+
+                if min_tenp > float(all_element[pos_min]):
+                    min_tenp = float(all_element[pos_min])
+                prez += float(all_element[pos_prez])
+                bb_tenp_min += float(all_element[pos_min])
+
+                if prev_month != month:
+                    bb_tenp_max /= cont
+                    bb_tenp_min /= cont
+                    prez /= cont
+                    if max_tenp == 0.0:
+                        max_tenp = 'null'
+                    if min_tenp == 0.0:
+                        min_tenp = 'null'
+                    if bb_tenp_max == 0.0:
+                        bb_tenp_max = 'null'
+                    if bb_tenp_min == 0.0:
+                        bb_tenp_min = 'null'
+                    if prez == 0.0:
+                        prez = 'null'
+
+                    if prev_month == "12":
+                        rows.append([prev_month + "/" + str(int(year)-1), max_tenp, bb_tenp_max, min_tenp,bb_tenp_min, prez])
+                    else:
+                        rows.append([prev_month + "/" + year, max_tenp, bb_tenp_max, min_tenp, bb_tenp_min, prez])
+
+                    max_tenp = -float("Inf")
+                    min_tenp = float("Inf")
+                    bb_tenp_max = 0.0
+                    bb_tenp_min = 0.0
+                    prez = 0.0
+                    cont = 0
+    bb_tenp_max /= cont
+    bb_tenp_min /= cont
+    prez /= cont
+    if max_tenp == 0.0:
+        max_tenp = 'null'
+    if min_tenp == 0.0:
+        min_tenp = 'null'
+    if bb_tenp_max == 0.0:
+        bb_tenp_max = 'null'
+    if bb_tenp_min == 0.0:
+        bb_tenp_min = 'null'
+    if prez == 0.0:
+        prez = 'null'
+
+    rows.append([prev_month + "/" + year, max_tenp, bb_tenp_max, min_tenp, bb_tenp_min, prez])
+
     text['data'] = rows
 
     # Serializing json
     json_object = json.dumps(text, indent=4)
+    # Writing to sample.json
+
+    with open(directory, "w") as outfile:
+        outfile.write(json_object)
+
+
+def convert_toJSON_AUT(directory, files):
+    rows = []
+    month = "01"
+    pos_max = 1
+    pos_min = 2
+    pos_prez = 5
+    pos_media = 3
+    pos_hez = 4
+    for f in files:
+        # open csv files
+        mycsv = csv.reader(open(f, encoding='latin-1'))
+
+        first = True
+        max_tenp = -float("Inf")
+        min_tenp = float("Inf")
+        bb_tenp_max = 0.0
+        bb_tenp_min = 0.0
+        prez = 0.0
+        cont = 0
+        bb = 0.0
+        hez = 0.0
+
+        for line in mycsv:
+            if first:
+                first = False
+            else:
+                cont += 1
+                # get all element in an array
+                all_element = "".join(line).split(";")  # [0] data, [1] max. temp, [2] min. temp, [3] prez.
+                all_element = ['0.0' if elem == '' else elem for elem in all_element]
+                year = all_element[0].split(" ")[0].split("/")[-1]
+                prev_month = month
+                month = all_element[0].split(" ")[0].split("/")[1]
+
+
+                if max_tenp < float(all_element[pos_max]):
+                    max_tenp = float(all_element[pos_max])
+                bb_tenp_max += float(all_element[pos_max])
+                if min_tenp > float(all_element[pos_min]):
+                    min_tenp = float(all_element[pos_min])
+                prez += float(all_element[pos_prez])
+                bb_tenp_min += float(all_element[pos_min])
+                bb += float(all_element[pos_media])
+                hez += float(all_element[pos_hez])
+
+                if prev_month != month:
+                    bb_tenp_max /= cont
+                    bb_tenp_min /= cont
+                    bb /= cont
+                    prez /= cont
+                    hez /= cont
+                    if max_tenp == 0.0:
+                        max_tenp = 'null'
+                    if min_tenp == 0.0:
+                        min_tenp = 'null'
+                    if bb_tenp_max == 0.0:
+                        bb_tenp_max = 'null'
+                    if bb_tenp_min == 0.0:
+                        bb_tenp_min = 'null'
+                    if prez == 0.0:
+                        prez = 'null'
+                    if bb == 0.0:
+                        bb = 'null'
+                    if hez == 0.0:
+                        hez = 'null'
+
+                    if prev_month == "12":
+                        rows.append(
+                            [prev_month + "/" + str(int(year) - 1), bb, max_tenp, bb_tenp_max, min_tenp, bb_tenp_min, hez, prez])
+                    else:
+                        rows.append([prev_month + "/" + year, bb, max_tenp, bb_tenp_max, min_tenp, bb_tenp_min, hez, prez])
+
+                    max_tenp = -float("Inf")
+                    min_tenp = float("Inf")
+                    bb = 0.0
+                    bb_tenp_max = 0.0
+                    bb_tenp_min = 0.0
+                    prez = 0.0
+                    hez = 0.0
+                    cont = 0
+    bb_tenp_max /= cont
+    bb_tenp_min /= cont
+    bb /= cont
+    prez /= cont
+    hez /= cont
+    if max_tenp == 0.0:
+        max_tenp = 'null'
+    if min_tenp == 0.0:
+        min_tenp = 'null'
+    if bb_tenp_max == 0.0:
+        bb_tenp_max = 'null'
+    if bb_tenp_min == 0.0:
+        bb_tenp_min = 'null'
+    if prez == 0.0:
+        prez = 'null'
+    if bb == 0.0:
+        bb = 'null'
+    if hez == 0.0:
+        hez = 'null'
+
+    rows.append([prev_month + "/" + year, bb, max_tenp, bb_tenp_max, min_tenp, bb_tenp_min, hez, prez])
+    text2['data'] = rows
+
+    # Serializing json
+    json_object = json.dumps(text2, indent=4)
     # Writing to sample.json
 
     with open(directory, "w") as outfile:
---
+++ mainAUT.py
@@ -0,0 +1,74 @@
+import os, csv
+from urllib.request import urlopen
+import bs4
+# 1. Import the requests library
+import requests
+import pandas as pd
+import glob
+import os
+from convertToJSON import *
+
+
+def createFile(path):
+    if not os.path.exists(path):
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+def createDirectory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+stations = ["ablitas%20mapa","abodi%20(pikatua)%20gn", "adiós%20mapa", "aguilar%20de%20codés%20gn", "aibar%20mapa", "aoiz%20gn", "aralar%20gn",
+    "arangoiti%20gn", "arazuri%20intia", "artajona%20mapa", "bardenas%20(barranco)%20intia", "bardenas%20(el%20plano)%20mapa", "bardenas%20(el%20yugo)%20gn",
+    "bardenas%20(loma%20negra)%20gn", "bargota%20mapa", "beortegi%20gn", "bera%20(larrategaña)%20gn", "cadreita%20intia", "carcastillo%20(la%20oliva)%20gn",
+    "carrascal%20gn", "cascante%20mapa", "corella%20intia", "doneztebe-santesteban%20gn", "el%20perdón%20gn", "eltzaburu%20gn", "erremendia%20(salazar)%20gn",
+    "estella%20gn", "etxalar%20(orizkiko%20lepoa)%20gn", "etxarri-aranatz%20gn", "falces%20mapa", "fitero%20mapa", "funes%20intia", "getadar%20gn", "goizueta%20gn",
+    "gorramendi%20gn", "iñarbegi%20gn", "irabia%20gn", "isaba-refugio%20belagua%20gn", "lerín%20intia", "los%20arcos%20mapa", "lumbier%20intia", "miranda%20de%20arga%20mapa",
+    "murillo%20el%20fruto%20mapa", "olite%20intia", "oskotz%20gn", "pamplona%20(etsia)%20upna", "pamplona%20gn", "san%20adrián%20mapa","san%20martín%20de%20unx%20mapa",
+    "sartaguda%20%20intia", "sartaguda%20gn", "sesma%20mapa", "tafalla%20gn","traibuenas%20intia", "trinidad%20de%20iturgoien%20gn", "tudela%20(montes%20del%20cierzo)%20gn",
+    "tudela%20(valdetellas)%20mapa", "ujué%20gn", "urbasa%20gn", "villanueva%20de%20yerri%20gn", "yesa%20gn", "ancín%20intia"]
+
+codes =["274", "519", "263", "33", "262", "34", "22", "23", "243", "264", "6", "275", "31", "26", "268", "12", "460", "4", "35", "24", "276", "258", "42", "28","461",
+        "249", "7", "517", "8", "269", "273", "259", "246", "499", "25", "497", "32", "502","267", "424", "247", "266", "270", "257", "37", "405", "455", "271", "265","5", "21", "423", "9",
+        "242", "29", "36", "272","30", "250", "11", "10", "251"]
+#WARNING! To get the last station info. is neccesary to change the years to [2015-2022]
+
+assert (len(stations) == len(codes))
+for j in range(len(stations)):
+    createDirectory("./results")
+    directory = "./results/" + stations[j]
+    createDirectory(directory)
+    csv_list = []
+    partial_link = "http://meteo.navarra.es/_data/datos_estaciones/estacion_"
+    partial_link2 = "/datos%20diarios/"
+    years = [str(i) for i in range(1880, 2022)] #2022 is not taking into account
+    i = 0
+    while i < len(years):
+        try:
+            # open the link
+            if "mapa" in stations[j] and int(years[i]) <= 2018:
+                link = partial_link + codes[j] + partial_link2 + stations[j] + "ma_" + years[i] + ".csv"
+            else:
+                link = partial_link + codes[j] + partial_link2 + stations[j] +"_" + years[i] + ".csv"
+            # 2. download the data behind the URL
+            response = requests.get(link)
+            # 3. Open the response into a new file
+            if "  <h2>404: archivo o directorio no encontrado.</h2>" not in str(response.content):
+                open(directory+"/"+years[i]+".csv", "wb").write(response.content)
+                csv_list.append(directory+"/"+years[i]+".csv")
+            i += 1
+        except:
+            print("error: an error occurs while opening the link: "+ link)
+            i += 1
+            pass
+    if len(csv_list) > 0:
+        #merge all the csv-s in one
+        # merging the files
+        # Return a list of all joined files
+        combined_csv = pd.concat([pd.read_csv(f, encoding='latin-1') for f in csv_list ])
+        #to .csv
+        combined_csv.to_csv(directory + "/" + stations[j] + ".csv", index=False, encoding='latin-1')
+
+        #convert to json
+        json_directory = directory + "/" + stations[j] + ".json"
+        createFile(json_directory)
+        convert_toJSON_AUT(json_directory, csv_list)
